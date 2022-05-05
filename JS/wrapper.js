e08307c0f1ab592e365a8540907097d752ddc9b4
xiaojiang <alway3501@gmail.com>
2022-05-05 13:54:00 +0800

    5/4

---
+++ JS/wrapper.js
@@ -0,0 +1,30 @@
+gsap.registerPlugin(ScrollTrigger);
+
+//---------------------Landing Page ScrollTrigger---------------------
+function LandingPageScrollTrigger() {
+
+    gsap.to('body', { // LoadingAnimation---------------------
+        opacity: 1, duration: 1.3
+    }) // /LoadingAnimation---------------------
+
+    let LandingPageScrollTrigger = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#ImgWrapper",
+            start: "0% 0%",
+            end: "100% 0%",
+            pin: "#ImgWrapper",
+            scrub: 2.2,
+        }
+    })
+    LandingPageScrollTrigger
+        
+    .to('#ImgWrapper #img', { y: -450, }, 0)
+    .to('#ImgWrapper #img', { z: 2800, }, 0)
+
+        .from('#codeby', { y : 130, opacity: 0 }, 0.31)
+}
+//---------------------/Landing Page ScrollTrigger---------------------
+
+window.onload = () => {
+    LandingPageScrollTrigger()
+}
